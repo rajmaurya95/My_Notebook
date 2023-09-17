7d3eec6641b63293d268e7514f2008efb6f04416
rajmaurya95 <gautamshakya9528@gmail.com>
2023-09-17 13:32:13 +0530

    Updates

---
+++ precache-manifest.b0cc8abb701aef915607564dc3cac62d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a98dc1959771443285f",
+    "url": "/my_Notebook/static/css/main.75806ce3.chunk.css"
+  },
+  {
+    "revision": "2a98dc1959771443285f",
+    "url": "/my_Notebook/static/js/main.2a98dc19.chunk.js"
+  },
+  {
+    "revision": "57fddca0c6f63fcbb255",
+    "url": "/my_Notebook/static/js/1.57fddca0.chunk.js"
+  },
+  {
+    "revision": "c0a29190e6c806e41712",
+    "url": "/my_Notebook/static/js/runtime~main.c0a29190.js"
+  },
+  {
+    "revision": "d7ccecca2e70eb95647b8b8227f399ae",
+    "url": "/my_Notebook/index.html"
+  }
+];
